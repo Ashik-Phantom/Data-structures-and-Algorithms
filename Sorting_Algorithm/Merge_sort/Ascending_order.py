58ae335560c724e61086742a05454e697f51cc4c
_Phantom_ <60980662+Ashik-Phantom@users.noreply.github.com>
2020-09-08 14:14:13 +0530

    Update Ascending_order.py

--- Sorting_Algorithm/Merge_sort/Ascending_order.py
+++ Sorting_Algorithm/Merge_sort/Ascending_order.py
@@ -2,26 +2,26 @@
 # MergeSort - Sort by Ascending order
 # This program get a input N and a array if input of size N and sorts the array in ascending order
   
-def merge_sort(values):  
-    if len(values) > 1: 
-        m = len(values)//2
-        left = values[:m] 
-        right = values[m:] 
+def merge_sort(arr):  
+    if len(arr) > 1: 
+        mid = len(arr)//2
+        left = arr[:mid] 
+        right = arr[mid:] 
         left = merge_sort(left) 
         right = merge_sort(right)   
-        values =[]   
-        while len(left)>0 and len(right)>0: 
+        arr =[]   
+        while len(left) > 0 and len(right) > 0: 
             if left[0]<right[0]: 
-                values.append(left[0]) 
+                arr.append(left[0]) 
                 left.pop(0) 
             else: 
-                values.append(right[0]) 
+                arr.append(right[0]) 
                 right.pop(0) 
         for i in left: 
-            values.append(i) 
+            arr.append(i) 
         for i in right: 
-            values.append(i)                   
-    return values 
+            arr.append(i)                   
+    return arr 
 
 n = int(input()) 
 a=list(map(int,input().split()))  
